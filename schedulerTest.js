1406b4fc2bc829bec0409196a348c2776304860a
Sagar <sagarshinde1600@gmail.com>
2025-07-29 17:00:28 +0530

    Update schedulerTest.js

--- schedulerTest.js
+++ schedulerTest.js
@@ -167,7 +167,7 @@ const runAllScrapers = async () => {
         if (allJobs.length > 0) {
             const enrichedJobs = allJobs.map(job => extractData(job));
             console.log("enrichedJobs=",enrichedJobs);
-            await sendToBackend(enrichedJobs);
+            //await sendToBackend(enrichedJobs);
             //console.log(`📤 Sent ${enrichedJobs.length} jobs to backend`);
         }
 
