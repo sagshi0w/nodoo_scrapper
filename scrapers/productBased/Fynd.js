b61cdb49ca099fc4825af3540e6a23411e1449e9
Sagar <sagarshinde1600@gmail.com>
2025-08-13 21:36:48 +0530

    Update Fynd.js

--- scrapers/productBased/Fynd.js
+++ scrapers/productBased/Fynd.js
@@ -40,7 +40,7 @@ class FyndJobsScraper {
 
             // Collect new links
             const jobLinks = await this.page.$$eval(
-                'a.card.job-card',
+                'a[href*="/careers/jobdetails/"][class*="job-card"]',
                 anchors => anchors.map(a => a.href)
             );
 
