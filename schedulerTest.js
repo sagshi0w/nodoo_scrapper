8d11c3e060851c401dbde74d69608d663c59ace3
Sagar <sagarshinde1600@gmail.com>
2025-10-05 09:34:40 +0530

    Update schedulerTest.js

--- schedulerTest.js
+++ schedulerTest.js
@@ -201,7 +201,7 @@ ${error.stack}`;
 
 const scrapers = [
     // { fn: OnePassword, headless: true },
-    { fn: GEA, headless: true },
+    { fn: runSiemensScraper, headless: true },
 ];
 
 const runAllScrapers = async () => {
